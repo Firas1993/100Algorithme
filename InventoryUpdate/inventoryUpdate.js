a273e43818f94200c61b8001a398ac807ddde6c8
Firas1993 <kebsi.firas@competitoor.com>
2019-12-13 20:28:58 +0100

    Inventory Update function, handle 2D array with js

---
+++ InventoryUpdate/inventoryUpdate.js
@@ -0,0 +1,54 @@
+function existInArray2D(arr,element){
+   return arr.map((item,index)=> item.includes(element));
+}
+function updateElment(arr1,element){
+    let existing = existInArray2D(arr1,element[1]);
+    let index = existing.indexOf(true);
+    arr1[index][0] += element[0]
+    return arr1; 
+}
+function updateInventory(arr1, arr2) {
+    // All inventory must be accounted for or you're fired!
+    let newArray = arr1;
+    for (let el in arr2){
+        let existing = existInArray2D(newArray,arr2[el][1]);
+        if(existing.includes(true)){
+            // if this elment exist in arr1 merge it in the the newArray
+            let exiProd = arr2[existing.indexOf(true)];
+            newArray = updateElment(newArray,exiProd);
+        }else{
+            // if it's not exist jsut push it in the newArray 
+            newArray.push(arr2[el]);
+        }
+    }
+    //sort array of 2D 
+    newArray.sort((a,b)=>{
+    a = a[1];b=b[1];
+    if (a > b) {
+        return 1;
+    }
+    if (b > a) {
+        return -1;
+    }
+    return 0;
+    })
+    console.log(newArray);
+    return newArray;
+}
+
+// Example inventory lists
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+//console.log(existInArray2D(newInv,curInv[0][1]));
+updateInventory(curInv, newInv);
--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,22 @@
-function absoluteValuesSumMinimization(a: number[]): number {
-
-}
+/*function absoluteValuesSumMinimization(a: number[]): number {
+   
+   return Math.abs()
+}*/
 
 // console.log(absoluteValuesSumMinimization([2, 4, 7]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+function FirstFactorial(num) { 
+
+  // code goes here
+  if(num === 0){ num = 1}
+  else {
+    num = num * FirstFactorial(num-1)
+  }
+  return num; 
+
+}
+   
+// keep this function call here 
+console.log(FirstFactorial(5));
